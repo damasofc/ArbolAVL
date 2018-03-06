e0a3639f65b608828bfbd4f8866338ea97c3c8d1
damasofc <damasofc@hotmail.com>
2018-03-05 19:23:03 -0600

    aca funciona, pero no al 100

--- main.cpp
+++ main.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 int main()
 {
-    ItemMemory* raiz = new ItemMemory(100,"Damaso","Sistemas");
-    ItemMemory* raiz2 = new ItemMemory(2,"Gabriel","admin");
-    ItemMemory* raiz3 = new ItemMemory(35,"Nelson","admin");
-    ItemMemory* raiz4 = new ItemMemory(40,"Iris","conta");
-    ItemMemory* raiz5 = new ItemMemory(5,"Jaun","conta");
+    ItemMemory* raiz = new ItemMemory(43,"Damaso","Sistemas");
+    ItemMemory* raiz2 = new ItemMemory(18,"Gabriel","admin");
+    ItemMemory* raiz3 = new ItemMemory(22,"Nelson","admin");
+    ItemMemory* raiz4 = new ItemMemory(9,"Iris","conta");
+    // ItemMemory* raiz5 = new ItemMemory(5,"Jaun","conta");
     //ItemMemory* raiz5 = new ItemMemory(5,"Jaun","conta");
     ArbolAvl* arbol = new ArbolAvl();
+    // arbol->cargarArbol("arbol2.dat");
     arbol->agregarNodo(raiz);
     arbol->agregarNodo(raiz2);
     arbol->agregarNodo(raiz3);
     arbol->agregarNodo(raiz4);
-    arbol->agregarNodo(raiz5);
-    //arbol->cargarArbol("arbolRotDoble.dat");
+    // arbol->agregarNodo(raiz5);
+    // arbol->guardarArbol("arbol2.dat");
+    // arbol->cargarArbol("arbolRotDoble.dat");
     arbol->imprimir();
 
     return 0;
